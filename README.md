854095c6fec04dfcafabc8bcbf65745fa62f4880
Frank Stokes <frank@wearereasonablepeople.com>
2021-06-06 10:16:07 +0200

    Use more efficient binary PPM format

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # WaveStrider
 
 WaveStrider is a hobby ray marching project, which I'm using to improve my C++ skills, and to learn more about 3d rendering.
+
+## Latest screenshot
+
+<img src="test.ppm">
--- src/ppm.cpp
+++ src/ppm.cpp
@@ -1,37 +1,41 @@
 #include <assert.h>
 #include <math.h>
+#include <string.h>
 #include "./ppm.hpp"
 
-uint32_t clamp(uint32_t vMin, uint32_t vMax, double v)
+uint8_t clamp(uint8_t vMin, uint8_t vMax, double v)
 {
   if (v < vMin) return vMin;
   if (v > vMax) return vMax;
-  return (uint32_t)(round(v));
+  return (uint8_t)(round(v));
 };
 
 bool WaveStrider::PPM::write(std::string filename, uint32_t width, uint32_t height, std::vector<WaveStrider::vec3> data)
 {
   assert(data.size() == width * height);
 
-  std::fstream file_out;
-  file_out.open(filename, std::ios_base::out);
-  if (!file_out.is_open()) {
+  std::ofstream f(filename, std::ofstream::binary);
+  if (!f.is_open()) {
     return false;
   }
 
-  file_out << "P3\n"
-           << width << " " << height << "\n255\n";
+  f.write("P6\n", 3);
+  f.write(std::to_string(width).c_str(), std::to_string(width).length());
+  f.write(" ", 1);
+  f.write(std::to_string(height).c_str(), std::to_string(height).length());
+  f.write("\n255\n", 5);
 
+  char tmp;
   for (vec3 color : data) {
-    file_out << clamp(0, 255, color.x * 255.0)
-             << " "
-             << clamp(0, 255, color.y * 255.0)
-             << " "
-             << clamp(0, 255, color.z * 255.0)
-             << " ";
+    tmp = clamp(0, 255, color.x * 255.0);
+    f.write(&tmp, 1);
+    tmp = clamp(0, 255, color.y * 255.0);
+    f.write(&tmp, 1);
+    tmp = clamp(0, 255, color.z * 255.0);
+    f.write(&tmp, 1);
   }
 
-  file_out.close();
+  f.close();
 
   return true;
 };
